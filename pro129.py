86ed31e41e8cdd5f6bc1e16219c655d6cc84d5ff
shrihan2008 <80960812+shrihan2008@users.noreply.github.com>
2022-08-07 18:19:25 +0530

    Add files via upload

--- pro129.py
+++ pro129.py
@@ -5,6 +5,10 @@ with open('dwarf_stars.csv','r') as f:
     for i in read:
         data.append(i)
 
+f=f.dropna()
+star_data=data[1:]
+headers=data[0]
+star_data.sort(key=lambda star_data:star_data[2])
 mass=f["Mass"]
 mass=float(mass)
 radius=f["Radius"]
@@ -12,11 +16,6 @@ radius=float(radius)
 radius=radius*0.102763
 mass=mass*0.000954588
 
-f=f.dropna()
-
-headers=data[0]
-
-star_data=data[1:]
 with open('browndwarfs_sorted.csv','a+') as f:
     write=csv.writer(f)
     write.writerow(headers)
